a28bd5cf2530266c57f7fecf62cb31ad45535530
Alec DiFederico <alecdifederico@gmail.com>
2018-04-22 23:09:43 -0400

    added admin dashboard and login/logout functionality

--- app/views.py
+++ app/views.py
@@ -1,6 +1,19 @@
 from flask import render_template, request, session, url_for, redirect, flash
+from functools import wraps
 
 from app import app
+
+
+# Decorator to check if admin is logged in
+def admin_logged_in(f):
+    @wraps(f)
+    def wrap(*args, **kwargs):
+        if 'admin_logged_in' in session:
+            return f(*args, **kwargs)
+        else:
+            flash('Access denied', 'danger')
+            return redirect(url_for('home'))
+    return wrap
 
 
 @app.route('/')
@@ -19,6 +32,19 @@ def admin_login():
         session['admin_logged_in'] = True
         flash('Admin is successfully logged in', 'success')
 
-        return redirect(url_for('home'))
+        return redirect(url_for('admin_dashboard'))
     return render_template('admin_login.html')
 
+
+@app.route('/dashboard')
+@admin_logged_in
+def admin_dashboard():
+    return render_template('admin_dashboard.html')
+
+
+@app.route('/logout')
+@admin_logged_in
+def logout():
+    session.clear()
+    flash('You have successfully logged out', 'success')
+    return redirect(url_for('home'))
