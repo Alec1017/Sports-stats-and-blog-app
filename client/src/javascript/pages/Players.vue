939e57b6e75af5b84d75bfcd3a3677c2d8f0f2bd
Alec <alecdifederico@gmail.com>
2019-07-17 01:34:10 -0400

    finished player cards

--- client/src/javascript/pages/Players.vue
+++ client/src/javascript/pages/Players.vue
@@ -1,23 +1,22 @@
 <template>
   <div class="players">
-    <div class="players__heading row">
-        <div class="players__title col-md-8 text-center">
-	        <h1>Players <small>2019 Season</small></h1>
-        </div>
+    <div class="players__heading">
+        <div class="players__title">Players</div>
     </div>
     <div class="players__grid">
         <!-- eslint-disable-next-line vue/require-v-for-key -->
-        <div class="players__player card" v-for="player in this.players">
-          <img class="players__image card-img-top" :src="player.profile_picture.url" :alt="player.captain[0].text" height="215">
-          <div class="players__content card-body">
-              <h5 class="players__card__title card-title">{{ player.captain[0].text }}</h5>
-              <p class="players__card__text card-text"><b>{{ player.role[0].text }}</b><br/>{{ player.description[0].text }}</p>
-          </div>
-          <div class="players__card__footer card-footer">
-              <router-link class="btn btn-primary" :to="`/stats/${player._meta.uid}`">2019 stats</router-link>
-          </div>
+        <div class="card" v-for="player in this.players">
+          <img class="card__image" :src="player.profile_picture.url" :alt="player.captain[0].text" height="215">
+          <div class="card__title">{{ player.captain[0].text }}</div>
+          <div class="card__role">{{ player.role[0].text }}</div>
+          <div class="card__description">{{ player.description[0].text }}</div>
+          <router-link class="card__button" :to="`/stats/${player._meta.uid}`">2019 stats</router-link>
+          <ul class="card__social_media">
+            <li><a href="#"><i class="fab fa-facebook-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-twitter-square"></i></a></li>
+            <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+          </ul>
         </div>
-      
     </div>
   </div>
 </template>
