23b3b48b21707a82d51e818e027233d5ac0f012d
Alec <alecdifederico@gmail.com>
2019-07-19 03:03:34 -0400

    added favicon

--- client/src/javascript/components/NavBar.vue
+++ client/src/javascript/components/NavBar.vue
@@ -2,26 +2,25 @@
   <div class="navbar">
     <div class="navbar__inner">
       <div class="navbar__logo">
-        <router-link to="/">WBL</router-link>
+        <router-link v-on:click.native="closeNav" to="/">WBL</router-link>
       </div>
-      <input class="navbar__toggle" id="navbar-toggle" type="checkbox">
-      <label class="navbar__hamburger" for="navbar-toggle">
+      <div class="navbar__hamburger" @click="toggleNav">
         <span></span>
-      </label>
-      <ul class="navbar__list">
+      </div>
+      <ul class="navbar__list" v-bind:class="{ isOpen: this.navOpen }">
         <li>
           <div class="navbar__link">
-            <router-link to="/feed">Feed</router-link>
+            <router-link v-on:click.native="closeNav" to="/feed">Feed</router-link>
           </div>
         </li>
         <li>
           <div class="navbar__link">
-            <router-link to="/players">Players</router-link>
+            <router-link v-on:click.native="closeNav" to="/players">Players</router-link>
           </div>
         </li>
         <li>
           <div class="navbar__link">
-            <router-link to="/standings">Standings</router-link>
+            <router-link v-on:click.native="closeNav" to="/standings">Standings</router-link>
           </div>
         </li>
         <li>
@@ -42,8 +41,21 @@
 
   export default {
     name: 'navBar',
+    data() {
+      return {
+        navOpen: false
+      }
+    },
     components: {
       TwitterSVG
+    },
+    methods: {
+      toggleNav() {
+        this.navOpen = !this.navOpen;
+      },
+      closeNav() {
+        this.navOpen = false;
+      }
     }
   }
 </script>
