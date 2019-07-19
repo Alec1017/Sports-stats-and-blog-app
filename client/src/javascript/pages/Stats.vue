d6da5c170b54b85b05d2bb80bf5b9742ebe121ce
Alec <alecdifederico@gmail.com>
2019-07-18 23:20:23 -0400

    version 1 of site finished

--- client/src/javascript/pages/Stats.vue
+++ client/src/javascript/pages/Stats.vue
@@ -2,15 +2,19 @@
   <div class="stats" v-if="playerExists">
 
     <img class="stats__image" :src="player.profile_picture.url" :alt="player.captain[0].text">
-    <div class="stats__name">{{ player.captain[0].text }}</div>
-    <div class="stats__info">Bat: {{player.batting_direction}} | Throw: {{player.throw_direction}} | height: {{player.height}}" | Age: {{player.age}}</div>
+    <div class="stats__basic_info">
+      <div class="stats__name">{{ player.captain[0].text }}</div>
+      <div class="stats__info">Bat: {{player.batting_direction}} | Throw: {{player.throw_direction}} | height: {{player.height}}" | Age: {{player.age}}</div>
 
-    <div class="stats__bio">
-      <div class="stats__bio_category">Status: <span class="stats__bio_data">{{player.status}}</span></div>
-      <div class="stats__bio_category">Debut: <span class="stats__bio_data">{{ parseInt(player.debut)}}</span></div>
-      <div class="stats__bio_category">Awards: <span class="stats__bio_data">{{player.awards}}</span></div>
-      <div class="stats__bio_category">championships: <span class="stats__bio_data">{{player.championship_titles}}</span></div>
+      <div class="stats__bio">
+        <div class="stats__bio_category">Status: <span class="stats__bio_data">{{player.status}}</span></div>
+        <div class="stats__bio_category">Debut: <span class="stats__bio_data">{{ parseInt(player.debut)}}</span></div>
+        <div class="stats__bio_category">Awards: <span class="stats__bio_data">{{player.awards}}</span></div>
+        <div class="stats__bio_category">championships: <span class="stats__bio_data">{{player.championship_titles}}</span></div>
+      </div>
     </div>
+
+   
 
     <div class="tab">
       <input class="tab__input tab__input--hitting" id="tab-hitting" type="radio" name="tabs" checked>
@@ -22,8 +26,9 @@
       <input class="tab__input tab__input--analytics" id="tab-analytics" type="radio" name="tabs">
       <label class="tab__label" for="tab-analytics">Analytics</label>
 
+      <!-- hitting tab -->
       <div class="tab__content tab__content--hitting">
-        <table class="table">
+        <table class="table stats__table stats__condensed-data">
           <thead class="table__head">
             <tr class="table__row">
               <th>H</th>
@@ -37,14 +42,49 @@
             <tr class="table__row">
               <td>{{ player.h }}</td>
               <td>{{ player.ab }}</td>
-              <td>{{ player.k }}</td>
               <td>{{ player.hr }}</td>
               <td>{{ player.rbi }}</td>
+              <td>{{ player.k }}</td>
             </tr>
           </tbody>
         </table>
 
-        <table class="table">
+        <table class="table stats__table stats__full-data">
+          <thead class="table__head">
+            <tr class="table__row">
+              <th>H</th>
+              <th>AB</th>
+              <th>1B</th>
+              <th>2B</th> 
+              <th>3B</th>
+              <th>HR</th>
+              <th>HBP</th>
+              <th>BB</th>
+              <th>RBI</th>
+              <th>K</th>
+              <th>SB</th>
+              <th>OUTS</th>
+            </tr>
+          </thead>
+          <tbody class="table__body">
+            <tr class="table__row">
+              <td>{{ player.h }}</td>
+              <td>{{ player.ab }}</td>
+              <td>{{ player.one_b }}</td>
+              <td>{{ player.two_b }}</td>
+              <td>{{ player.three_b }}</td>
+              <td>{{ player.hr }}</td>
+              <td>{{ player.hbp }}</td>
+              <td>{{ player.bb }}</td>
+              <td>{{ player.rbi }}</td>
+              <td>{{ player.k }}</td>
+              <td>{{ player.sb }}</td>
+              <td>{{ player.outs }}</td>
+            </tr>
+          </tbody>
+        </table>
+
+        <table class="table stats__table">
           <thead class="table__head">
             <tr class="table__row">
               <th>AVG</th>
@@ -64,8 +104,9 @@
         </table>
       </div>
 
+      <!-- pitching tab -->
       <div class="tab__content tab__content--pitching">
-        <table class="table">
+        <table class="table stats__table stats__condensed-data">
           <thead class="table__head">
             <tr class="table__row">
               <th>IP</th>
@@ -86,7 +127,7 @@
           </tbody>
         </table>
 
-        <table class="table">
+        <table class="table stats__table stats__condensed-data">
           <thead class="table__head">
             <tr class="table__row">
               <th>ERA</th>
@@ -104,133 +145,42 @@
             </tr>
           </tbody>
         </table>
+
+        <table class="table stats__table stats__full-data">
+          <thead class="table__head">
+            <tr class="table__row">
+              <th>IP</th>
+              <th>ER</th>
+              <th>R</th>
+              <th>ERA</th>
+              <th>K</th>
+              <th>BB</th>
+              <th>W</th>
+              <th>L</th>
+              <th>SV</th>
+            </tr>
+          </thead>
+          <tbody class="table__body">
+            <tr class="table__row">
+              <td>{{ player.ip }}</td>
+              <td>{{ player.er }}</td>
+              <td>{{ player.r }}</td>
+              <td v-html="calcERA"></td>
+              <td>{{ player.pitching_k }}</td>
+              <td>{{ player.pitching_bb }}</td>
+              <td>{{ player.pitching_wins }}</td>
+              <td>{{ player.pitching_losses }}</td>
+              <td>{{ player.sv }}</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
 
+      <!-- analytics tab -->
       <div class="tab__content tab__content--analytics">
         Analytics will becoming soon!
       </div>
     </div>
-     
-
-
-
-
-
-   <!-- <div class="row">
-      <div class="stats__bio col-3">
-        <h6>Status: <span style="color: gray">{{player.status}}</span></h6>
-        <h6>Debut: <span style="color: gray">{{player.debut}}</span></h6>
-        <h6>Awards: <span style="color: gray">{{player.awards}}</span></h6>
-        <h6>championships: <span style="color: gray">{{player.championship_titles}}</span></h6>
-      </div>
-      <div class="col-9">
-        <ul class="nav nav-tabs">
-          <li class="nav-item">
-            <a class="nav-link active" data-toggle="tab" href="#hitting">Hitting</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="#pitching">Pitching</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" data-toggle="tab" href="#analytics">Analytics</a>
-          </li>
-        </ul>
-
-       
-       <div class="tab-content">
-
-          <div id="hitting" class="tab-pane fade show active">
-            <table class="stats__table table table-striped table-bordered">
-              <thead>
-                <tr>
-                  <th scope="col" class="text-center">H</th>
-                  <th scope="col" class="text-center">AB</th>
-                  <th scope="col" class="text-center">1B</th>
-                  <th scope="col" class="text-center">2B</th>
-                  <th scope="col" class="text-center">3B</th>
-                  <th scope="col" class="text-center">HR</th>
-                  <th scope="col" class="text-center">HBP</th>
-                  <th scope="col" class="text-center">BB</th>
-                  <th scope="col" class="text-center">RBI</th>
-                  <th scope="col" class="text-center">K</th>
-                  <th scope="col" class="text-center">SB</th>
-                  <th scope="col" class="text-center">OUTS</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{{ player.h }}</td>
-                  <td>{{ player.ab }}</td>
-                  <td>{{ player.one_b }}</td>
-                  <td>{{ player.two_b }}</td>
-                  <td>{{ player.three_b }}</td>
-                  <td>{{ player.hr }}</td>
-                  <td>{{ player.hbp }}</td>
-                  <td>{{ player.bb }}</td>
-                  <td>{{ player.rbi }}</td>
-                  <td>{{ player.k }}</td>
-                  <td>{{ player.sb }}</td>
-                  <td>{{ player.outs }}</td>
-                </tr>
-              </tbody>
-            </table>
-
-            <table class="stats__table table table-striped table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col" class="text-center">AVG</th>
-                        <th scope="col" class="text-center">OBP</th>
-                        <th scope="col" class="text-center">SLG</th>
-                        <th scope="col" class="text-center">OPS</th>
-                    </tr>
-                </thead>
-                <tbody>
-                  <tr>
-                    <td v-html="calcAVG"></td>
-                    <td v-html="calcOBP"></td>
-                    <td v-html="calcSLG"></td>
-                    <td v-html="calcOPS"></td>
-                  </tr>
-                </tbody>
-            </table>
-          </div>
-
-          <div id="pitching" class="tab-pane fade">
-            <table class="stats__table table table-striped table-bordered">
-              <thead>
-                <tr>
-                  <th scope="col" class="text-center">IP</th>
-                  <th scope="col" class="text-center">ER</th>
-                  <th scope="col" class="text-center">R</th>
-                  <th scope="col" class="text-center">K</th>
-                  <th scope="col" class="text-center">BB</th>
-                  <th scope="col" class="text-center">SV</th>
-                  <th scope="col" class="text-center">W</th>
-                  <th scope="col" class="text-center">L</th>
-                  <th scope="col" class="text-center">ERA</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td>{{ player.ip }}</td>
-                  <td>{{ player.er }}</td>
-                  <td>{{ player.r }}</td>
-                  <td>{{ player.pitching_k }}</td>
-                  <td>{{ player.pitching_bb }}</td>
-                  <td>{{ player.sv }}</td>
-                  <td>{{ player.pitching_wins }}</td>
-                  <td>{{ player.pitching_losses }}</td>
-                  <td v-html="calcERA"></td>
-                </tr>
-              </tbody>
-            </table>
-          </div>
-          <div id="analytics" class="tab-pane fade">
-            <h3>Coming soon to a theater near you</h3>
-          </div>
-        </div>
-      </div> 
-    </div> -->
   </div> 
 </template>
 
